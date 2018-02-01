24bcab9fdc717162fbacffc33413f3ac8db1b1d5
Jakub Kulich <jakubkulich@gmail.com>
2018-02-01 15:03:37 +0100

    chore(): removed grpc descriptor plugin

--- gen-angular-client/gen.sh
+++ gen-angular-client/gen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+rm /source/ui/api/models/*
 ngx-swag-client -s /source/swagger.yaml -o /source/ui/api
--- gen-go-grpc-desc/hooks/gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for service in "$@"
-do
-    echo "Generating gRPC server and stubs from protofile for service $service"
-    cd /source/$service/protobuf
-    protoc -I/usr/local/include -I. -I/googleapis -I${GOPATH}/src --gogo_out=plugins=grpc:/source/$service proto.proto
-    protoc -I/usr/local/include -I. -I/googleapis -I${GOPATH}/src --include_imports --include_source_info --descriptor_set_out /source/$service/protobuf/descriptor.pb proto.proto
-done
